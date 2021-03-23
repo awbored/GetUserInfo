29c9954494cff9f4c04b922696024d5094707270
Andrew Wayne <79549187+awbored@users.noreply.github.com>
2021-03-23 11:39:39 -0700

    Add files via upload

---
+++ GetUserInfo.ps1
@@ -0,0 +1,6 @@
+﻿$ID = $args[0]
+IF($ID -eq $null){$ID = Read-Host -Prompt "What is the sAMAccountName?"}
+$attrib = Get-Content .\attrib.txt
+
+Get-ADUser -Identity $ID -Properties $attrib |
+Select-Object $attrib
